e27a34386e32cc4126e6cdbb480e435c9b044dfc
Sagar Mahure <Sagarmahure325@gmail.com>
2025-10-15 13:49:01 +0530

    1.py commited

---
+++ Assignment 1/1.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter first no: "))
+num2 = int(input("Enter Second no: "))
+
+print ("Addition of 2 numbers is", num1 + num2 )    # Addition of 2 no
+print (f"Subtraction of 2 numbers is {num1-num2}")  # Subtraction of 2 no
