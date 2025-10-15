365eea5c3a0096d0f9b76bc175d9133230e89a64
Sagar Mahure <Sagarmahure325@gmail.com>
2025-10-15 14:31:41 +0530

    added python files from 3 to 6

---
+++ Assignment 1/4.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter First No: "))
+num2 = float(input("Enter Second No: "))
+
+result = num1/num2
+
+print(f"The Division of 2 numbers is {result}")
---
+++ Assignment 1/5.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter First No : "))
+num2 = int(input("Enter Second No : "))
+
+result = num1 % num2
+
+print(f"Remainder after First No dived by Second is {result}")
