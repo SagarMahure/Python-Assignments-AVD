b0bda0fd8a8d2f6c51371a32466bf7436c17f42b
SagarMahure <sagarmahure325@gmail.com>
2025-10-21 19:51:29 +0530

    21.py added

---
+++ Assignment 1/21.py
@@ -0,0 +1,11 @@
+m1 = int(input("Enter no : "))
+m2 = int(input("Enter no : "))
+m3 = int(input("Enter no : "))
+m4 = int(input("Enter no : "))
+m5 = int(input("Enter no : "))
+
+total = m1+m2+m3+m4+m5
+avg = total/5
+
+print(f"The Total marks of student is {total}, percentage is {(total/500)*100} and \
+the average marks is {avg}")
